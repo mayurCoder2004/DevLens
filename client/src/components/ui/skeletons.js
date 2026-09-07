73a70aaffe39e96b5a7c8392473418c56c966824
mayurCoder2004 <mayursomnathpawar123@gmail.com>
2026-09-08 00:41:31 +0530

    chore: remove unused skeleton components

--- client/src/components/ui/skeletons.js
+++ client/src/components/ui/skeletons.js
@@ -1,21 +1,6 @@
 /**
- * Skeleton Components Index
- *
- * Centralized exports for all skeleton loading components.
- * Import skeletons from this file for cleaner imports.
- *
- * @example
- * import { CardSkeleton, StatCardSkeleton } from '@/components/ui/skeletons';
+ * Shared skeleton component exports.
  */
 
-// Base Skeletons
-export { default as Skeleton } from "./Skeleton";
-export { default as CardSkeleton } from "./CardSkeleton";
-export { default as StatCardSkeleton } from "./StatCardSkeleton";
 export { default as HeroSkeleton } from "./HeroSkeleton";
-export { default as TableRowSkeleton } from "./TableRowSkeleton";
-export { default as ListSkeleton } from "./ListSkeleton";
-export { default as ChartSkeleton } from "./ChartSkeleton";
-export { default as MetricGridSkeleton } from "./MetricGridSkeleton";
-export { default as RepositoryCardSkeleton } from "./RepositoryCardSkeleton";
 export { default as ArchitectureGraphSkeleton } from "./ArchitectureGraphSkeleton";
